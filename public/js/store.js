ed9f854d2ab1b12e54c132ab9f75c09572664fb2
Empty Arsenal <matt.merkes@gmail.com>
2014-03-31 16:55:17 -0700

    Fixed the collection and getbyid route methods to get proper data to front end

--- public/js/store.js
+++ public/js/store.js
@@ -1,14 +1,8 @@
-/*
-App.Store = DS.Store.extend({
-   adapter: DS.FixtureAdapter
-});
-*/
 App.ApplicationAdapter = DS.RESTAdapter;
 
 DS.RESTAdapter.reopen({
     namespace: 'api/v1'
 });
+
 App.Store = DS.Store.extend({});
-App.ApplicationSerializer = DS.RESTSerializer.extend({
-  primaryKey: "_id"
-});
+
