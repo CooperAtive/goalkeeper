4de340c111c9cd7af551071ce755d66f24167114
Empty Arsenal <matt.merkes@gmail.com>
2014-04-07 12:12:11 -0700

    Fixed the runTarget controller to update model when a new event is added

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,8 @@ window.App = Ember.Application.create();
 
 App.Route = Ember.Route.extend({
   beforeModel: function() {
-    if(!this.get('router.url')) {
+    if(this.controllerFor('application').get('logout') || !this.get('router.url')) {
+      this.controllerFor('application').set('logout', false);
       return;
     } else if (!localStorage.user_id) {
       return this.transitionTo('login');
