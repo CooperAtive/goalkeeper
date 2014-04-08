911a71127b6b93b981aeeb760a04817fa4c77e94
Cooper Trowbridge <cooper.trowbridge@gmail.com>
2014-04-08 16:42:23 -0700

    target tests working

--- public/js/controllers/targetsCreateController.js
+++ public/js/controllers/targetsCreateController.js
@@ -1,18 +1,18 @@
 'use strict';
 
 App.TargetsCreateController = Ember.ObjectController.extend({
-  actions: {
-    createTarget: function() {
-      var self = this;
+    actions: {
+        createTarget: function() {
+            var self = this;
 
-      var model = this.get('model');
-      model.set('user_id', localStorage_userId);
-        model.save()
-        .then(function() {
-          self.transitionToRoute('runTarget', self.get('model'));
-        }, function() {
-          alert('Target not successfully saved');
-      });
+            var model = this.get('model');
+            model.set('user_id', localStorage.userId);
+            model.save()
+            .then(function() {
+                self.transitionToRoute('runTarget', self.get('model'));
+            }, function() {
+                alert('Target not successfully saved');
+            });
+        }
     }
-  }
 });
