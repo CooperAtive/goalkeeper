f8b012b927148d1986b1fbb2aa4e722649b3a091
Kalina Wu <kalina.wong@gmail.com>
2014-04-09 08:10:58 -0700

    html update

--- public/js/views/indexView.js
+++ public/js/views/indexView.js
@@ -1,4 +1,33 @@
 App.IndexView = Ember.View.extend({
   didInsertElement: function() {
+
+    $( "#goal1" ).animate({
+      left: "+=22%"
+    }, 2000, function() {
+      // Animation complete.
+    });
+
+    $( "#goal2" ).animate({
+      left: "-=10%"
+    }, 2000, function() {
+      // Animation complete.
+    });
+
+    $( "#goal3" ).animate({
+      left: "+=10%"
+    }, 2000, function() {
+      // Animation complete.
+    });
+
+    $( "#goal4" ).animate({
+      left: "-=20%"
+    }, 2000, function() {
+      // Animation complete.
+    });
+
+    $( "#goal-circle1" ).fadeIn(5000, function() {
+      // Animation complete
+    });
+
   }
 });
