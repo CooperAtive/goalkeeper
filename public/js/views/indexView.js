5a2b8c1c392413a4b45ff5055868c19a9bb9e915
Empty Arsenal <matt.merkes@gmail.com>
2014-04-30 10:35:28 -0700

    Index, login, and signup pages are mobile responsive

--- public/js/views/indexView.js
+++ public/js/views/indexView.js
@@ -1,11 +1,11 @@
 App.IndexView = Ember.View.extend({
   didInsertElement: function() {
 
-    $( "#goal1" ).delay(500).animate({left: "+=22%"}, 3000);
-    $( "#goal2" ).delay(500).animate({left: "-=10%"}, 3000);
-    $( "#goal3" ).delay(500).animate({left: "+=10%"}, 3000);
-    $( "#goal4" ).delay(500).animate({left: "-=20%"}, 3000);
+    $( "#goal1" ).delay(500).animate({left: "+=22%"}, 1000);
+    $( "#goal2" ).delay(500).animate({left: "-=10%"}, 1000);
+    $( "#goal3" ).delay(500).animate({left: "+=10%"}, 1000);
+    $( "#goal4" ).delay(500).animate({left: "-=20%"}, 1000);
 
-    $(".goal-circle").hide(1).delay(2499).fadeIn(3000);
+    $(".goal-circle").hide(1).delay(1000).fadeIn(1000);
   }
 });
